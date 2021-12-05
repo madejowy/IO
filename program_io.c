8aa6e5a591bc0ac96d91bec8427f64416bbcdea4
Maciej Madej <61713218+madejowy@users.noreply.github.com>
2021-12-05 11:24:08 +0100

    Add files via upload

---
+++ program_io.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+	if (argc == 1) {
+		printf("Usage: %s <key>", argv[0]);
+		return -1;
+	}
+	char flag[] = "CTF{qwertyuiop1235}";
+	char key[] = "Inzynieria_Odwrotna";
+
+	if (strcmp(key, argv[1]) == 0) {
+		printf("Correct key!\nFlag: %s", flag);
+	}
+	else {
+		printf("Incorrect key!");
+	}
+	return 0;
+}
